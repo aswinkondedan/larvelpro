3729ef458d1cd29313a3a64e22b80414974507c5
aswin kondedan <aswin6804@gmail.com>
2021-11-29 10:50:19 +0530

    first

---
+++ albicelest/database/migrations/2021_11_23_093638_create_profile_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Name');
+            $table->bigInteger('Age');
+            $table->string('Club');
+            $table->string('Picture');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile');
+    }
+}
---
+++ albicelest/resources/views/table.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.js"></script>
+</head>
+<body>
+
+<div align="center">
+    <h1>player details</h1>
+    <form action="" method="post" >
+
+        <label for="name">Name:</label><br>
+        <input type="text" id="name" name="name" value=""><br>
+        <label for="age">Age:</label><br>
+        <input type="number" id="age" name="age" value=""><br>
+        <label for="fname">Club:</label><br>
+        <input type="text" id="club" name="club" value=""><br><br>
+        <label for="picture">Picture:</label><br><br>
+        <input type="file" id="pic" name="pic" value=""><br><br>
+        <button type="submit">add</button>
+        
+    </form>
+    
+
+</div>
+    
+</body>
+s
+</html>
